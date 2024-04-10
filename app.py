c45492b70a3ec905c76f019cfe8aed776c21da85
Samip Shah <95608626+snarkyidiot@users.noreply.github.com>
2024-04-10 11:33:49 +0530

    Improved

--- app.py
+++ app.py
@@ -7,27 +7,30 @@ import pickle
 from sklearn import tree
 import pandas as pd
 from time import time
+from functools import reduce
 with open("model.pkl",'rb') as f:
     clf=pickle.load(f)
 
 def read(im_or):
   t1=time()
   gray = cv.cvtColor(im_or, cv.COLOR_BGR2GRAY)
-  thresh_98,im_1 = cv.threshold(gray, 100, 185, cv.THRESH_OTSU)
+  thresh_98,im_1 = cv.threshold(gray, 100, 255, cv.THRESH_OTSU)
   im_2=cv.copyMakeBorder(src=im_1, top=15, bottom=15, left=15, right=15, borderType=cv.BORDER_CONSTANT,value=0)
-  contours_98, hierarchy_98  = cv.findContours(im_2, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
+  contours_98, hierarchy_98  = cv.findContours(im_2, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+#   contours_98 =list(contours_98)
   t=0
-  ans=''
+  ans=[]
   while(t!=-1):
-    ans+=checkdigit(contours_98[t])
+    ans.append(checkdigit(contours_98[t]))
     t=hierarchy_98[0][t][0]
-  print(time()-t1)
-  return ans
+  if(len(ans)==0):return ""
+  ans.sort(key= lambda x:x[0])
+  return reduce(lambda x,y:x+y[1],ans,"")
 def checkdigit(cnt):
-    if cv.contourArea(cnt)<10:return "" 
+    if cv.contourArea(cnt)<30:return (2000,"") 
     mom=cv.moments(cnt)
     Preds=clf.predict([pd.Series(mom)])
-    return str(Preds[0])
+    return (int(mom['m10']/mom['m00']),str(Preds[0]))
 
 demo = gr.Interface(read, gr.Image(), "text")
 if __name__ == "__main__":
